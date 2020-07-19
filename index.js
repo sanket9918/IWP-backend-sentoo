fd17c2502bfc2c4b32d7fcc34f7ce892a23917e3
Sanket Mohapatra <sanketmohapatra13@gmail.com>
2020-07-19 14:57:01 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const user = require('./routes/user');
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+app.use(bodyParser.json());
+const mongoURI = require('./config/key').mongoURL;
+
+mongoose
+    .connect(mongoURI,
+        {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        }).then(() => {
+            console.log('MongoDB connected successfully');
+        }).catch((e) => {
+            console.log('MongoDB cannot connect');
+        })
+app.use('/api/user', user);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server is up and running at ${port}`);
+})
